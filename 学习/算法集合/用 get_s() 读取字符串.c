25fe2d888c50bae35956ff5574c98f73b8e8d594
Go-loneliness-of-man <1786053661@qq.com>
2018-04-29 16:26:32 +0800

    maopao_order

--- 学习/算法集合/用 get_s() 读取字符串.c
+++ 学习/算法集合/用 get_s() 读取字符串.c
@@ -3,7 +3,7 @@ char a[100];
 int main()
 {
 	printf("请输入字符: ");
-	gets_s(a, 100);					// get_s() 需要额外一个参数指出字符串的长度
+	gets_s(a, 100);					// gets_s() 需要额外一个参数指出字符串的长度
 	printf("%s\n", a);
 	return 0;
 }
